c30b61837f7782d0237c254ac50229f6c2dbea68
Samyak Jain <samyak24jain@gmail.com>
2015-11-29 10:29:47 +0530

    Added New Responsive Design

---
+++ New Design/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Paper Quest</title>
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <meta name="descripiton" content="Question Paper Forum for NITK Surathkal">
+        <meta name="authors" content="Samyak Jain, Sangeetha Grama Srinivasan">
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:500,300,700,400|Oxygen' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <section class="bg-blue" id="headline">
+            <div class="container">
+                <div class="row aligncenter">
+                    <div class="span12">
+                        <div class="aligncenter">
+                            <h1>PAPER QUEST</h1>
+                            <p>A Question Paper Forum for the students of NITK, Surathkal.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row aligncenter">
+                    <div class="span12">
+                        <h3>How This Works?</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-dark-grey">
+            <div class="container">
+                <div class="row list">
+                    <div class="span3">
+                        <div class="icons icons-colored round red">
+                            <i class="fa fa-cloud-download"></i>
+                        </div>
+                        <div class="content">
+                            <h3 class="uppercase aligncenter">Download</h3>
+                            <p class="aligncenter">Download the question papers you need.</p>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div class="icons icons-colored round green">
+                            <i class="fa fa-cloud-upload"></i>
+                        </div>
+                        <div class="content">
+                            <h3 class="uppercase aligncenter">Upload</h3>
+                            <p class="aligncenter">Upload useful question papers(P.S. - Only for CRs).</p>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div class="icons icons-colored round yellow">
+                            <i class="fa fa-sign-in"></i>
+                        </div>
+                        <div class="content">
+                            <h3 class="uppercase aligncenter">Register/Login</h3>
+                            <p class="aligncenter">Login now to upload papers.</p>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div class="icons icons-colored round blue">
+                            <i class="fa fa-exclamation-circle"></i>
+                        </div>
+                        <div class="content">
+                            <h3 class="uppercase aligncenter">Report</h3>
+                            <p class="aligncenter">Report papers you didn't find and need right way.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
